5928e2447ea4f9ba1551745d07873d3e89b0016e
7vTr0x7 <7vtrox7@gmail.com>
2024-03-25 15:43:31 +0530

    todo Items

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,6 @@
-import { Paper } from "@mui/material";
+import { Button, Checkbox, Paper, Stack, Typography } from "@mui/material";
+
+import { Delete } from "@mui/icons-material";
 
 type PropType = {
   todo: TodoItemType;
@@ -7,7 +9,16 @@ type PropType = {
 const TodoItem = ({ todo }: PropType) => {
   return (
     <div>
-      <Paper>app</Paper>
+      <Paper sx={{ padding: "1rem" }}>
+        <Stack direction={"row"} alignItems={"center"}>
+          <Typography marginRight={"auto"}>{todo.title}</Typography>
+          <Checkbox />
+          <Button sx={{ color: "Black" }}>Edit</Button>
+          <Button sx={{ color: "Black" }}>
+            <Delete />
+          </Button>
+        </Stack>
+      </Paper>
     </div>
   );
 };
