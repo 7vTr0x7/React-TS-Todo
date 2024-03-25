dcfbd1a5f20ff15ff7d591675d3f625e40a8f921
7vTr0x7 <7vtrox7@gmail.com>
2024-03-25 15:30:00 +0530

    todo Items

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import TodoItem from "./components/TodoItem";
+import { useState } from "react";
 
 const App = () => {
+  const [todos, setTodos] = useState<TodoItemType[]>([
+    { id: "1", title: "Todo 1", isCompleted: false },
+    { id: "2", title: "Todo 2", isCompleted: true },
+  ]);
+
   return (
     <div>
-      <Container maxWidth="sm">
+      <Container maxWidth="sm" sx={{ height: "85vh" }}>
         <AppBar position="static">
           <Toolbar>
             <Typography>Todo App</Typography>
           </Toolbar>
         </AppBar>
+
+        <Stack height={"80%"} direction={"column"} spacing={"1rem"} p={"1rem"}>
+          {todos.map((item) => (
+            <TodoItem key={item.id} todo={item} />
+          ))}
+        </Stack>
+        <TextField fullWidth label={"New Task"} />
+        <Button sx={{ margin: "1rem 0" }} fullWidth variant="contained">
+          Add
+        </Button>
       </Container>
     </div>
   );
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+import { Paper } from "@mui/material";
+
+type PropType = {
+  todo: TodoItemType;
+};
+
+const TodoItem = ({ todo }: PropType) => {
+  return (
+    <div>
+      <Paper>app</Paper>
+    </div>
+  );
+};
+
+export default TodoItem;
