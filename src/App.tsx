2ad16677a9b556f3ec4265eda739fe9e6baa82c8
7vTr0x7 <7vtrox7@gmail.com>
2024-03-25 16:06:29 +0530

    submit handler

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,25 @@ import TodoItem from "./components/TodoItem";
 import { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState<TodoItemType[]>([
-    { id: "1", title: "Todo 1", isCompleted: false },
-    { id: "2", title: "Todo 2", isCompleted: true },
-  ]);
+  const [todos, setTodos] = useState<TodoItemType[]>([]);
 
-  const completeHandler = (id: TodoItemType["id"]) => {};
+  const [title, setTitle] = useState<TodoItemType["title"]>("");
 
-  const deleteHandler = (id: TodoItemType["id"]) => {};
+  const completeHandler = (id: TodoItemType["id"]): void => {
+    alert(id);
+  };
+
+  const deleteHandler = (id: TodoItemType["id"]): void => {};
+  const submitHandler = (): void => {
+    const newTodo: TodoItemType = {
+      title,
+      isCompleted: false,
+      id: String(Math.floor(Math.random() * 100)),
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+    setTitle("");
+  };
 
   return (
     <div>
@@ -39,8 +50,18 @@ const App = () => {
             />
           ))}
         </Stack>
-        <TextField fullWidth label={"New Task"} />
-        <Button sx={{ margin: "1rem 0" }} fullWidth variant="contained">
+        <TextField
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          fullWidth
+          label={"New Task"}
+        />
+        <Button
+          disabled={title === ""}
+          onClick={submitHandler}
+          sx={{ margin: "1rem 0" }}
+          fullWidth
+          variant="contained">
           Add
         </Button>
       </Container>
